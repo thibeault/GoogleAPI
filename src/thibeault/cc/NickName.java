c62d88383ce1fe74639e06d1268d0e92bb8e6435
Eric Thibeault <thibeault@gmail.com>
2011-11-21 00:19:51 -0700

    is this really working?

--- src/thibeault/cc/NickName.java
+++ src/thibeault/cc/NickName.java
@@ -226,7 +226,7 @@ public class NickName {
 	  
 	  
 	  /**
-	   * Driver for the sample..
+	   * Driver for the sample.. is this really working
 	   */
 	  public void run() throws Exception {
 		  
